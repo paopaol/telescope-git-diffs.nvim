5374b47ef8c508554c91794f4c739a0a5d43d5dc
paopaol <jinzhao115@163.com>
2024-12-03 14:22:21 +0800

    use --graph

--- lua/telescope/_extensions/git_diffs.lua
+++ lua/telescope/_extensions/git_diffs.lua
@@ -2,10 +2,24 @@ local finders = require('telescope.finders')
 local pickers = require('telescope.pickers')
 local previewers = require "telescope.previewers"
 local conf = require('telescope.config').values
-local make_entry = require "telescope.make_entry"
+local make_entry = require "make_entry"
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 local utils = require "telescope.utils"
+
+
+M = {}
+
+
+
+local setup_opts = {
+  git_command = { "git", "log", "--graph", "--oneline", "--decorate", "--all", "." },
+  enable_preview_diff = true
+}
+
+M.setup = function(opts)
+  setup_opts = vim.tbl_deep_extend("force", setup_opts, opts)
+end
 
 
 local function diffview(prompt_bufnr)
@@ -15,33 +29,54 @@ local function diffview(prompt_bufnr)
   actions.close(prompt_bufnr)
 
 
-  if #selections ~= 2 then
-    utils.notify("diff_commits", { level = "WARN", msg = "must select 2 commits" })
+  if #selections > 2 then
+    utils.notify("diff_commits", { level = "WARN", msg = "must select 1 or 2 commits" })
     return
   end
 
+  -- Sort by date
+  table.sort(selections, function(a, b)
+    return tonumber(vim.fn.systemlist("git show -s --format=%ct " .. a.value)[1]) <
+        tonumber(vim.fn.systemlist("git show -s --format=%ct " .. b.value)[1])
+  end)
 
-  local new = string.sub(selections[1].value, 1, 8)
-  local old = string.sub(selections[2].value, 1, 8)
-  vim.cmd(string.format("DiffviewOpen -uno %s %s", old, new))
+
+  local old = #selections == 0 and string.sub(action_state.get_selected_entry().ordinal, 1, 7) or
+      string.sub(selections[1].value, 1, 8)
+
+  if #selections == 2 then
+    local new = string.sub(selections[2].value, 1, 8)
+    vim.cmd(string.format("DiffviewOpen %s..%s", old, new))
+  else
+    vim.cmd(string.format("DiffviewOpen %s^!", old))
+  end
+
   vim.cmd([[stopinsert]])
 end
 
-local function diff_commits(opts)
+M.diff_commits = function(opts)
   opts = opts or {}
   opts.entry_maker = vim.F.if_nil(opts.entry_maker, make_entry.gen_from_git_commits(opts))
 
+  local previewer_list = {}
 
-  local git_command = { "git", "log", "--oneline", "--decorate", "--all", "." }
-  pickers.new(opts, {
-    prompt_title = opts.prompt_title or "git diff_commits",
-    finder = finders.new_oneshot_job(git_command, opts),
-    previewer = {
+  if setup_opts.enable_preview_diff then
+    previewer_list = {
       previewers.git_commit_diff_to_parent.new(opts),
       previewers.git_commit_diff_to_head.new(opts),
       previewers.git_commit_diff_as_was.new(opts),
+    }
+  else
+    previewer_list = {
       previewers.git_commit_message.new(opts),
-    },
+    }
+  end
+
+
+  pickers.new(opts, {
+    prompt_title = opts.prompt_title or "git diff_commits",
+    finder = finders.new_oneshot_job(setup_opts.git_command, opts),
+    previewer = previewer_list,
     sorter = conf.generic_sorter(opts),
     attach_mappings = opts.attach_mappings or function(_, map)
       actions.select_default:replace(diffview)
@@ -51,7 +86,8 @@ local function diff_commits(opts)
 end
 
 return require('telescope').register_extension {
+  setup = M.setup,
   exports = {
-    diff_commits = diff_commits,
+    diff_commits = M.diff_commits,
   }
 }
